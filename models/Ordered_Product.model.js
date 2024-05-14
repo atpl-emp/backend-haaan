652a08574222f33dcdc80b5d587c637b4380856b
tilak-atpl <tilak.ram@autopeepal.com>
2024-05-14 20:53:05 +0530

    made two more api to get ,delete

--- models/Ordered_Product.model.js
+++ models/Ordered_Product.model.js
@@ -1,17 +1,19 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
+function generateRandomNumber() {
+  return Math.floor(Math.random() * 5) + 2; // Generates random number between 2 and 6
+}
+const orderedProductSchema = mongoose.Schema({
+  brand: { type: String, required: true },
+  MRP: { type: Number, required: true },
+  finalPrice: { type: Number, required: true },
+  img: { type: String, required: true },
+  orderedDate: { type: Date, default: Date.now },
+  expectedDeliveryDate: { type: Number, default: generateRandomNumber },
+});
+const OrderedProductModel = mongoose.model(
+  "ordered_product",
+  orderedProductSchema
+);
 
-const orderedProductSchema = mongoose.Schema(
-    {
-        brand:{type:String, required:true},
-        MRP :{type:Number, required:true},
-        finalPrice :{type:Number, required:true},
-        img:{type:String, required:true},
-        orderedDate: { type: Date, default: Date.now }
-       
-    }
-)
-const OrderedProductModel = mongoose.model("ordered_product", orderedProductSchema);
-
-
-module.exports = {OrderedProductModel}
+module.exports = { OrderedProductModel };
--- routes/orderProductRouter.js
+++ routes/orderProductRouter.js
@@ -11,7 +11,7 @@ OrderedProductRouter.post(
   "/ordered_product",
   authentication,
   async (req, res) => {
-    const { brand, MRP, finalPrice, img,orderedDate } = req.body;
+    const { brand, MRP, finalPrice, img, orderedDate } = req.body;
     console.log(req.body);
 
     const new_product = new OrderedProductModel({
@@ -26,4 +26,34 @@ OrderedProductRouter.post(
     res.status(200).send("Product ordered successfully.");
   }
 );
+
+OrderedProductRouter.get(
+  "/get-ordered-list",
+  authentication,
+  async (req, res) => {
+    try {
+      const products = await OrderedProductModel.find();
+      res.status(200).json(products);
+    } catch (error) {
+      console.error("Error fetching products:", error);
+      res.status(500).json({ error: "Internal server error" });
+    }
+  }
+);
+
+// Delete a specific product
+OrderedProductRouter.delete("/delete/:id", authentication, async (req, res) => {
+  const { id } = req.params;
+  try {
+    const deletedProduct = await OrderedProductModel.findByIdAndDelete(id);
+    if (!deletedProduct) {
+      return res.status(404).json({ error: "Product not found" });
+    }
+    res.status(200).json({ message: "Product deleted successfully" });
+  } catch (error) {
+    console.error("Error deleting product:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
 module.exports = { OrderedProductRouter };
